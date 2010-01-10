1762794e4d78d2439a4f088e1f4b8ab227350e32
Patrick Ulbrich <zulu99@gmx.net>
2010-01-10 16:15:12 +0100

    added CdCollect import skeleton makefiles regenerated

---
+++ VolumeDB/src/Import/CdCollectImport.cs
@@ -0,0 +1,32 @@
+// CdCollectImport.cs
+// 
+// Copyright (C) 2010 Patrick Ulbrich
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+
+namespace VolumeDB.Import
+{
+	public sealed class CdCollectImport : AbstractImport
+	{
+		public CdCollectImport(VolumeDatabase targetDb, string dbDataPath)
+		: base(targetDb, dbDataPath) {}
+		
+		protected override void ImportThreadMain(VolumeDatabase targetDb, string dbDataPath) {
+			throw new NotImplementedException();
+		}
+	}
+}
