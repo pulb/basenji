56a3bf4296d949ef557915f9cd542e2707dbbf17
Patrick Ulbrich <zulu99@gmx.net>
2011-10-08 16:57:34 +0200

    fixed copyright data of AssemblyInfos

--- Platform.Common.DB/src/AssemblyInfo.cs
+++ Platform.Common.DB/src/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 // AssemblyInfo.cs
 // 
-// Copyright (C) 2008 Patrick Ulbrich
+// Copyright (C) 2008 - 2011 Patrick Ulbrich
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
